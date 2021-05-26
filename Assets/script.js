c7fd678612eddece0e951dfaaedbc31ee012a8b3
SaquebKhan <82435053+SaquebKhan@users.noreply.github.com>
2021-05-26 17:38:34 -0400

    Final update

--- Assets/script.js
+++ Assets/script.js
@@ -12,30 +12,39 @@ timeUpdater();
 function timeUpdater(){
 
     var currentTime = moment().hours();
-    $(".time-display").each(function(){
+    $(".time-block").each(function(){
         var blockHour = parseInt($(this).attr("id").split("-")[1]);
         if (blockHour < currentTime) {
             $(this).addClass("past");
         }
-        else if (blockHour === currentHour){
+        else if (blockHour == currentHour){
             $(this).removeClass("past");
             $(this).addClass("present");
         }
         else{
             $(this).removeClass("past");
             $(this).removeClass("present");
-            $(this).addClass("past");
+            $(this).addClass("future");
         }
-    })
+    });
 }
 })
 
 // load things in from local storage, repeat once it works
 $("#hour-8 .description").val(localStorage.getItem("hour-8"));
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
 //repeat for all hours
 
 //Add date to top using Moment JS
 
-$('#currentDay').text(moment().format("LOOK AT MOMENT DOCUMENTATION AND CHOOSE THE FORMAT THAT YOU LIKE"))
+$('#currentDay').text(moment().format("MMMM Do YYYY, h:mm:ss a"))
 
 // })
